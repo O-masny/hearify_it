b4a5ee431141e2b8a118548b9025d47a18db9b96
o-masny <icelyy@seznam.cz>
2021-10-26 00:01:40 +0200

    adding fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// @dart=2.9
-
 import 'package:flutter/material.dart';
 import 'package:hearify_it/pages/page_root.dart';
 
@@ -10,4 +8,38 @@ void main() {
       home: RootApp(),
     ),
   );
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurpleAccent,
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 25, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 20, fontFamily: 'Hind'),
+          caption: TextStyle(fontSize: 10, fontStyle: FontStyle.italic),
+          bodyText1: TextStyle(fontSize: 15, fontStyle: FontStyle.italic),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: RootApp(),
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
